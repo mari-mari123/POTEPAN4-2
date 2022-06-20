a3375e67f78ec4fa13dbc534109acba3447c3551
marie.y <18l1135h@gmail.com>
2022-06-21 00:16:29 +0900

    [add]新規（ファイル）機能追加 deviseを使ってユーザーの認証システムを作りました

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  get 'reservations/index'
+  post 'reservations/confirm'
+  get 'reservations/confirm'
+  post 'reservations/show' => 'reservations#create'
+  get 'reservations/show'
+  root 'homes#index'
+  get 'users/account'
+  get 'users/profile'
+  patch 'users/profile' => 'users#update'
+  get 'rooms/posts'
+  get 'rooms/search/result' => 'rooms#search'
+  resources :rooms
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
